d74246204c4014499e9ba707c9a770cd4902fb9b
sakuratyan <1105429691@qq.com>
2018-12-01 23:54:35 +0800

    add .gitignore

---
+++ c+c++/gloabAndLocalVar.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int aG = 100;
+
+int main(){
+	cout << "HelloWorld"<<endl;
+	int iL = 10;
+	cout <<"&aGL: "<<&aG<<endl;
+	cout<<"&iL: "<< &iL<< endl;  
+	return 0;
+}
+
---
+++ shelltest/for2.sh
@@ -0,0 +1,8 @@
+
+
+
+#!/bin/sh
+for i in hello 1 * 2 goodbye 
+do
+	echo "Looping ... i is set to $i"
+done
